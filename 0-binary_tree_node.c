77943b96407e052edd1802999f3fd7a9de61c565
Chima-01 <divinechima76@gmail.com>
2023-08-30 13:39:40 -0700

    insert a node to left side of parent

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -15,11 +15,18 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 	new_node = malloc(sizeof(binary_tree_t));
 	if (!new_node)
 		return (NULL);
+
+	new_node->parent = parent;
 	new_node->n = value;
 	new_node->left = NULL;
 	new_node->right = NULL;
-	if (parent != NULL)
-		new_node->parent = parent;
 
+	if (parent != NULL)
+	{
+		if (parent->left == NULL)
+			parent->left = new_node;
+		else
+			parent->right = new_node;
+	}
 	return (new_node);
 }
