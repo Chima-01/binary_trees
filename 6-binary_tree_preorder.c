d98f6ade542d421a394fd339ec25e812e1d95ec6
Chima-01 <divinechima76@gmail.com>
2023-08-31 01:50:29 -0700

    traversing a tree using different method

---
+++ 6-binary_tree_preorder.c
@@ -0,0 +1,18 @@
+#include "binary_trees.h"
+
+/**
+ *  binary_tree_preorder - A function that goes through
+ *  a binary tree using pre-order traversal
+ *  @tree: tree to traverse
+ *  @func: function pointer
+ */
+
+void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
+{
+	if (!tree)
+		return;
+
+	func(tree->n);
+	binary_tree_preorder(tree->left, func);
+	binary_tree_preorder(tree->right, func);
+}
