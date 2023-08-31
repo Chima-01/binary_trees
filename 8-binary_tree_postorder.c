5d472741e801848af2d3ffbca48b5ebe946bf6b6
Chima-01 <divinechima76@gmail.com>
2023-08-31 02:01:43 -0700

    fixed error task 7, 8, 9

--- 8-binary_tree_postorder.c
+++ 8-binary_tree_postorder.c
@@ -1,15 +1,15 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_postorder - A  function that goes through a binary tree using post-order
- * traversal
+ * binary_tree_postorder - A  function that goes through a binary tree
+ * using post-order traversal
  * @tree: tree to traverse
  * @func: pointer to a function
  */
 
 void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
 {
-	if (!tree)
+	if (!tree || !func)
 		return;
 
 	binary_tree_postorder(tree->left, func);
