a595745ae24b0ae837ff557f932eae276bff6bb5
Chima-01 <divinechima76@gmail.com>
2023-08-31 12:10:18 -0700

    height of node from index 0

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -1,6 +1,28 @@
 #include "binary_trees.h"
 
 /**
+ * max - Calculate the height of tree
+ * @tree: tree to traverse
+ * Return: height
+ */
+
+size_t max(const binary_tree_t *tree)
+{
+	size_t left_height, right_height;
+
+	if (!tree)
+		return (0);
+
+	left_height = max(tree->left);
+	right_height = max(tree->right);
+
+	if (left_height > right_height)
+		return (left_height + 1);
+	else
+		return (right_height + 1);
+}
+
+/**
  * binary_tree_height - Calculate the height of tree
  * @tree: tree to claculate height of
  * Return: height of tree or 0 if tree is NULL
@@ -8,16 +30,8 @@
 
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	size_t left_height, right_height;
-
 	if (!tree)
 		return (0);
 
-	left_height = binary_tree_height(tree->left);
-	right_height = binary_tree_height(tree->right);
-
-	if (left_height > right_height)
-		return (left_height + 1);
-	else
-		return (right_height + 1);
+	return (max(tree) - 1);
 }
