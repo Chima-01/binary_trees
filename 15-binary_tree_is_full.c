a91b37aa63798c146fe751348be2f59c8e5dda25
Chima-01 <divinechima76@gmail.com>
2023-08-31 21:17:29 -0700

    updated task 15

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -1,12 +1,26 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_is_full - This function check if a binary tree is created
- * @tree: tree to traverse
- * Return: 0 false else 1
+ * binary_tree_is_full - This function check if a binary tree is null
+ * @tree: tree to examine
+ * Return: 0 if tree is null
  */
 
 int binary_tree_is_full(const binary_tree_t *tree)
+{
+	if (!tree)
+		return (0);
+
+	return (check_binary_full(tree));
+}
+
+/**
+ * check_binary_full - This function check if a binary tree is full
+ * @tree: tree to traverse
+ *  Return: 0 false else 1
+ */
+
+int check_binary_full(const binary_tree_t *tree)
 {
 	int full_left, full_right;
 
@@ -17,8 +31,8 @@ int binary_tree_is_full(const binary_tree_t *tree)
 		(tree->left != NULL && !tree->right))
 		return (0);
 
-	full_left = binary_tree_is_full(tree->left);
-	full_right = binary_tree_is_full(tree->right);
+	full_left = check_binary_full(tree->left);
+	full_right = check_binary_full(tree->right);
 
 	return (full_left && full_right);
 }
